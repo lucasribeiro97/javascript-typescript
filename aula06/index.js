2d6194d04085d62847b33579ef4ab8d2defb29e7
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-12-07 17:48:11 -0300

    Fiz a aula 06

---
+++ aula06/index.js
@@ -0,0 +1,14 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não pode conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// NÃO UTILIZE VAR, UTILIZE LET.
+let nomeCliente = 'Lucas';
+let nomecliente = 'Ribeiro';
+
+console.log(nomeCliente);
+console.log(nomecliente);
+
