210f4ccc23ab351291f88e8a562c391083bd3279
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-12-08 18:08:20 -0300

    Fiz a aula 11

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,1 +1,3 @@
-alert('Com a nossa mensagem.');
+const num1 = Number(window.prompt('Digite um número:'));
+const num2 = Number(window.prompt('Digite outro número:'))
+alert(`O resultado da operação foi ${num1 + num2}`)
