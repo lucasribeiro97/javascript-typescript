f94d85e158ed32c361fa608499ba12d9159667da
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-12-08 16:46:32 -0300

    Fiz a aula 10

---
+++ aula10/index.js
@@ -0,0 +1,20 @@
+const nome = 'Lucas'; // string
+const nome1 = "Lucas"; // string
+const nome2 = `Lucas`; // string
+
+const num1 = 10; // number
+const num2= 10.52 // number
+
+let nomeAluno; // undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> Não aponta pra local nenhum na memória
+
+const aprovado =  false; // Boolean = true, false (lógico)
+
+let a = 2;
+let b = a;
+
+console.log(a, b);
+
+a = 3
+console.log(a, b);
+
