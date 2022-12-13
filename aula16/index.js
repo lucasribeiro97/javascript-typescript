5ddd58db2ab996e03ce1962f636c47a2c18c7686
Lucas Ribeiro Cunha <76178670+lucasribeiro97@users.noreply.github.com>
2022-12-13 11:40:33 -0300

    Fiz a aula 16

---
+++ aula16/index.js
@@ -0,0 +1,33 @@
+//               0       1        2
+let alunos = ['Luiz', 'Maria', 'João'];
+alunos = 123;
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos[50]);
+
+// const removido = alunos.shift();
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Luiza'); Adiciona no começo
+// alunos.unshift('Fabio');
+
+// alunos.push('Luiza'); Adiciona no fim
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza'; Adiciona no fim
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+// alunos[0] = 'Eduardo'; Altera
+// alunos[3] = 'Luiza';
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
+
